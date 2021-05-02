aa38808cb3c983a9ca4b1f042aa20eefaa90cda1
nahee-park <jessica396@ewhain.net>
2021-05-02 17:41:42 +0900

    styling1-인풋창쉐도우 해결안된버전

--- 3rd_semina/src/components/SearchBar.js
+++ 3rd_semina/src/components/SearchBar.js
@@ -1,4 +1,54 @@
 import React from 'react';
+import styled from 'styled-components';
+import searchIcon from '../images/search-icon.svg';
+
+const SearchBarWrap = styled.div`
+display:flex;
+flex-direction: column;
+align-items: center;
+margin-top: 35vh;
+form{
+    width: 591px;
+    height: 69px;
+    left: 424px;
+    top: 415px;
+
+    background: #414141;
+    border: 8px solid rgba(105, 105, 105, 0.5);
+    box-shadow: 0px 4px 15px rgba(0, 0, 0, 0.25);
+    border-radius: 40px;
+    display: flex;
+    align-items: center;
+    color: #F9F9F9;
+}
+input{
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+    border-radius : 0; 
+    background: #414141;
+
+    font-family: Noto Sans;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 1.25rem;
+    line-height: 132.5%;
+    /* or 26px */
+
+    display: flex;
+    align-items: center;
+
+    color: #F9F9F9;
+
+    text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+}
+input::placeholder {
+    color: #F9F9F9;
+}
+input:focus {
+    outline: none;
+}
+`;
 
 const SearchBar = ({getData}) => {
     const [userName, setUserName] = React.useState(null);
@@ -15,14 +65,17 @@ const SearchBar = ({getData}) => {
         event.preventDefault();
         getData(userName);
     }
-    return(
-        <>
-        <form onSubmit={submitHandler}>
-            <input type="text" value={userName} onChange={ChangeHandeler} placeholder="Github ID를 입력해주세요"></input>
-        </form>
 
-        </>
+    
+    return(
+        <SearchBarWrap>
+            <form onSubmit={submitHandler}>
+                <img src={searchIcon} />
+                <input type="text" value={userName} onChange={ChangeHandeler} placeholder="Github ID를 입력해주세요"></input>
+            </form>
+        </SearchBarWrap>
     );
 };
 
 export default SearchBar;
+
--- 3rd_semina/src/components/UserCard.js
+++ 3rd_semina/src/components/UserCard.js
@@ -1,17 +1,24 @@
 import React from "react";
+import styled from "styled-components";
+
+const UserCardWrap = styled.div`
+`;
 
 const UserCard = ({ data }) => {
     //맨 처음엔 userData가 빈값이므로 userData가 undefined일 땐 로딩중 출력되도록
     return (
         data && (
-            <>
+            <UserCardWrap>
             <img src={data.avatar_url} />
-            <p>{data.name}</p>
-            <p>{data.bio}</p>
-            <p>{data.followers}</p>
-            <p>{data.following}</p>
-            <p>{data.login}</p>
-            </>
+            <p>Name: {data.name}</p>
+            <p>Id: {data.login}</p>
+            <p>Bio: {data.bio}</p>
+            <div className="result-list">
+                <p>Followers: {data.followers}</p>
+                <p>Followings: {data.following}</p>
+                <p>Repository: {data.public_repos}</p>    
+            </div>
+            </UserCardWrap>
         )
     );
 };
