29ebce5b2a5b02634e8c8254eee31248789e1f7a
nahee-park <jessica396@ewhain.net>
2021-06-11 20:44:38 +0900

    feat: calendar 추가, 반응형 추가

--- 2nd_semina/clock-weather/calender.js
+++ 2nd_semina/clock-weather/calender.js
@@ -1,69 +1,104 @@
-const calenderHeader = document.querySelector(".calender-header");
-const calenderWeek = document.querySelector(".calender-week");
-const calenderDate = document.querySelector(".calender-date");
-const now = new Date();
-const week=now.getDay(); //0
-const date=now.getDate(); //25
-const month = now.getMonth(); 
+const calendarHeader = document.querySelector(".calendar-header");
+const calendarWeek = document.querySelector(".calendar-week");
+const calendarDate = document.querySelector(".calendar-date");
 
-console.log(now.getDate()); //25
-console.log(now.getDay()); //0
-console.log(month); //3
-
-
-function drawMonth(month){
-    const monthList = [
-        "January",
-        "February",
-        "March",
-        "April",
-        "May",
-        "June",
-        "July",
-        "August",
-        "September",
-        "October",
-        "November",
-        "December",
-    ];
-    month = monthList[month];
-    calenderHeader.innerHTML = `${monthList[month]}`;
+//월별 날짜수 세팅
+function getMaxDate(month) {
+  switch (month + 1) {
+    case 2:
+      return 28;
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+      return 30;
+    default:
+      return 31;
+  }
 }
 
-function drawWeek(){
-    const week = ["S", "M", "T", "W", "T", "F", "S"];
-    week.forEack((day)=>{
-        const eachDay = document.createElement("div")
-        eachDay.innerText = day;
-        calenderWeek.appendChild(eachDay);
-    })
+//요일 세팅
+function getStartWeek(date, week) {
+  if (date > week) {
+    date %= 7;
+    if (date === 0) week += 1;
+    else week = (week - date + 8) % 7;
+  }
+  return week;
 }
 
-function getMaxDate(month){
-    switch (month + 1) {
-        case 2:
-            return 28;
-        case 4:
-        case 6:
-        case 9:
-        case 11:
-            return 30;
-        default:
-            return 31;
+//일 그림
+function drawDate(month, date, week) {
+  const maxDate = getMaxDate(month);
+  const startWeek = getStartWeek(date, week);
+  console.log(startWeek);
+  let dateLine = document.createElement("div"); // 달력 줄
+  for (let i = 1; i <= startWeek + maxDate; i++) {
+    const dateEl = document.createElement("div"); // 날짜 칸
+
+    // 달력 맨 처음의 빈 부분
+    if (i <= startWeek) {
+      dateLine.appendChild(dateEl);
+      // 날짜 칸
+    } else {
+      dateEl.innerText = i - startWeek;
+      // 오늘 날짜 표시
+      if (i - startWeek === date) dateEl.style.color = "rgb(218, 76, 76)";
+      dateLine.appendChild(dateEl);
+
+      // 달력 줄바꿈
+      if (i % 7 === 0) {
+        calendarDate.appendChild(dateLine);
+        dateLine = document.createElement("div");
+      } else if (i === startWeek + maxDate) {
+        calendarDate.appendChild(dateLine);
+      }
     }
-};
-
-function drawDate(month, date, week){
-    
+  }
 }
 
+//월 그림
+function drawMonth(month) {
+  const monthList = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  console.log(monthList);
+  console.log("month", month);
+  month = monthList[month];
+  console.log("new month", month);
+  calendarHeader.innerText = `${month}`;
+}
 
+//요일 그림
+function drawWeek() {
+  const week = ["S", "M", "T", "W", "T", "F", "S"];
+  week.forEach((week) => {
+    const eachWeek = document.createElement("div");
+    eachWeek.innerText = week;
+    calendarWeek.appendChild(eachWeek);
+  });
+}
 
-function drawAll(){
-    drawMonth(month);
-    drayweek();
-    draydate(date);
+function drawAll() {
+  const now = new Date();
+  const week = now.getDay();
+  const date = now.getDate();
+  const month = now.getMonth();
+
+  drawMonth(month);
+  drawWeek();
+  drawDate(month, date, week);
 }
 
 drawAll();
-
